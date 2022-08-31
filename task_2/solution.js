17d7a9b64a5b30393f5d3b63bd0e245350a77e92
Galina Topalidi <galinaat.73@gmail.com>
2022-08-31 22:37:32 +0300

    task done

--- task_2/solution.js
+++ task_2/solution.js
@@ -16,6 +16,12 @@ function calcShipping(sum, min, shipping) {
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
 
+let shippingSum;
+  if (productsSum == 0 || productsSum >= freeShippingMinSum) 
+  {shippingSum = 0;}  
+  else (productsSum > 0 || productsSum < freeShippingMinSum) 
+  {shippingSum = shippingPrice;}
+
     // Конец решения задания №2.1.
 
     return shippingSum;
@@ -34,6 +40,11 @@ function calcDiscount(sum, min, discount) {
     // то присвойте discountSum значение discountPart процентов от productsSum,
     // иначе присвойте discountSum значение 0
 
+    let discountSum;
+    if (productsSum >= discountMinSum)
+    {discountSum = discountPart * productsSum / 100}
+    else {discountSum = 0;}
+
     // Конец решения задания №2.2.
 
     return discountSum;
@@ -50,7 +61,15 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     // присвойте totalSum значение productsSum
     // уменьшите totalSum на discountSum
 
+ let totalSum;
+   totalSum = productsSum;
+   totalSum = totalSum - discountSum;
+
     let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
+
+ totalSum = totalSum + shippingSum;
+   let freeShipping;
+   freeShipping = !Boolean(shippingSum);
 
     // прибавьте к totalSum значение shippingSum
 
