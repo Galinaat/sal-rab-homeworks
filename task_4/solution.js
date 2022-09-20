2a78aaebef9cee2f0f19fab7804e33521732b028
Galinaat <111235587+Galinaat@users.noreply.github.com>
2022-09-20 16:28:12 +0300

    Update solution.js

--- task_4/solution.js
+++ task_4/solution.js
@@ -30,7 +30,7 @@ return products;
 
 function renderProductsCards(json) {
 clearProducts();
-let product = parseProducts(json);
+let products = parseProducts(json);
 length = products.length;
 for (let i = 0; i < length; i +=1) {
 addProduct(products[i]);
